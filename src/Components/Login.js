9eb1bc263039d0898db41ee8ecd335e3b07a98aa
mfatima7 <fatimamamedova2007@mail.ru>
2024-07-25 01:31:53 -0400

    log-in page completed

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,7 +14,7 @@ function Login() {
     <div className="login">
       <div className="login-container" onClick={checkData}>
         <div class="login-logo">
-          <i class="fa-brands fa-spotify"></i>
+          <img src={image.minilogo} />
         </div>
         <h1>Log in to Spotify</h1>
         <div class="login-buttons">
@@ -24,14 +24,44 @@ function Login() {
           <button className="login-button facebook">
             <img src={image.facebook}></img>Continue with Facebook
           </button>
-          <button className="login-button apple">   <img src={image.apple}></img> Continue with Apple</button>
+          <button className="login-button apple">
+            {" "}
+            <img src={image.apple}></img> Continue with Apple
+          </button>
         </div>
-        <hr className="line" ></hr>
+        <hr className="line"></hr>
         <form class="login-form">
-          <input type="text" placeholder="Email or username" required />
-          <input type="password" placeholder="Password" required />
-          <button type="submit">Log In</button>
+          <div className="email">
+            <p>Email or username</p>
+            <input type="text" placeholder="Email or username" required />
+          </div>
+          <div className="password">
+            <p>Password</p>
+            <input type="password" placeholder="Password" required />
+          </div>
+          {/* <div className="rememberme" >
+            
+          </div> */}
+
+          <button typeof="submit" className="login-btn">
+            Log in
+          </button>
+          <a href="#" className="forgetPassword">
+            Forget your password?
+          </a>
         </form>
+        <hr className="line"></hr>
+        <p className="acc">
+          Don't have an account?  
+          <a href="" className="signup-link">
+              Sign up for Spotify
+          </a>
+        </p>
+      </div>
+      <div className="logfooter">
+        This site is protected by reCAPTCHA and the Google
+        <a href=""> Privacy Policy </a> and <a href=""> Terms of Service </a>
+        apply.
       </div>
     </div>
   );
--- src/Images/Images.js
+++ src/Images/Images.js
@@ -2,9 +2,11 @@ import logo from './logo.png'
 import google from './google.png'
 import facebook from './facebook.png'
 import apple from './apple.png'
+import minilogo from './minilogo.png'
 export const image =  {
     logo,
     google,
     facebook,
-    apple
+    apple,
+    minilogo
 }
